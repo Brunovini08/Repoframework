665dbb260f34104fdc302521080aa30833558abe
Brunovini08 <brunovinicius082005@gmail.com>
2026-01-15 16:14:54 -0300

    feat: Adicionando interface para o contrato onde é obrigatório passar um tipo de retorno para aquela conexão, por exemplo: SqlConnection

---
+++ Repoframework/Interfaces/IConfigurationDB.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repoframework.Repository.Interfaces
+{
+    public interface IConfigurationDB<TConnection>
+    {
+        public TConnection Connection();
+    }
+}
