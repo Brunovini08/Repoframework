5b071dc343ed190b8b01ca54a471fc962702597d
Brunovini08 <brunovinicius082005@gmail.com>
2026-01-16 14:31:58 -0300

    refactor: Refatorando a classe DatabaseSqlServer onde agora o método Connection retorna um IDbConnection para ser utilizado em outra classe

--- Repoframework/Implementations/Data/SqlServer/DatabaseSqlServer.cs
+++ Repoframework/Implementations/Data/SqlServer/DatabaseSqlServer.cs
@@ -1,102 +1,33 @@
 ﻿using Dapper;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 using Repoframework.Repository.Interfaces;
+using System.Data;
 
-namespace Repoframework.Repository.Implementations.Data
+namespace Repoframework.Repository.Implementations.Data.SqlServer
 {
     public class DatabaseSqlServer : IConfigurationDB
     {
+        private SqlConnection? _connection;
+        private string _connectionString;
 
-        private readonly string _connectionString;
-        private SqlConnection _connection;
-
-        public DatabaseSqlServer(string connectionString)
+        public DatabaseSqlServer(IOptions<DatabaseOptions> options)
         {
-            _connectionString = connectionString;
+            _connectionString = options.Value.ConnectionString;
         }
 
-        public void Connection()
+        public IDbConnection Connection()
         {
-            _connection = new SqlConnection(_connectionString);
+            return new SqlConnection(_connectionString);
         }
 
         public void Dispose()
         {
-            if (_connection is not null)
-            {
-                _connection.Dispose();
-                _connection = null;
-            }
+            _connection?.Dispose();
+            _connection = null;
         }
 
-        public async Task Create(object formatObject, string sql)
-        {
-            try
-            {
-                if (_connection is null)
-                    throw new InvalidOperationException("Database connection is not established. Call Connection() method first.");
-                await _connection.ExecuteAsync(sql, formatObject);
-            }
-            catch (SqlException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
-        public Task Delete(object formatObject, string sql)
-        {
-            try
-            {
-                if (_connection is null)
-                    throw new InvalidOperationException("Database connection is not established. Call Connection() method first.");
-                return _connection.ExecuteAsync(sql, formatObject);
-            }
-            catch (SqlException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
-        public async Task<IEnumerable<T>> GetAll<T>(string sql)
-        {
-            try
-            {
-                if (_connection is null)
-                    throw new InvalidOperationException("Database connection is not established. Call Connection() method first.");
-                return await _connection.QueryAsync<T>(sql);
-            }
-            catch (SqlException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
-        public async Task<T> GetByIdentifier<T>(object formatObject, string sql)
-        {
-            try
-            {
-                if (_connection is null)
-                    throw new InvalidOperationException("Database connection is not established. Call Connection() method first.");
-                return await _connection.QueryFirstOrDefaultAsync(sql, formatObject);
-            }
-            catch (SqlException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
-        public Task Update(string sql, object formatObject)
-        {
-            try
-            {
-                if (_connection is null)
-                    throw new InvalidOperationException("Database connection is not established. Call Connection() method first.");
-                return _connection.ExecuteAsync(sql, formatObject);
-            }
-            catch (SqlException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
+        public IDbConnection GetConnection() => _connection;
     }
 }
